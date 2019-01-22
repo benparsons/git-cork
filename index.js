f5eb777bbea6c45eb0d7ca4dec5a4ecb130b4fa3
Ben Parsons <ben@bpulse.co.uk>
2019-01-22 22:35:43 +0000

    correct exit code syntax

--- index.js
+++ index.js
@@ -15,20 +15,20 @@ function gitParse(path) {
     if (err) {
       console.log("\n\n=======\n" + path);
       console.log(`err: ${err}`);
-      process.exitCode(1);
+      process.exitCode = 1;
       return;
     }
     if (stderr) {
       console.log("\n\n=======\n" + path);
       console.log(`stderr: ${stderr}`);
-      process.exitCode(1);
+      process.exitCode = 1;
       return;
     }
 
     if (stdout.indexOf('ahead') !== -1 || stdout.indexOf('behind') !== -1 || stdout.split('\n').length > 2) {
       console.log("\n\n=======\n" + path);
       console.log(`stdout: ${stdout}`);
-      process.exitCode(1);
+      process.exitCode = 1;
     }
   });
 }
